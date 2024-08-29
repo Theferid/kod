8434f4f9a706cb514d5895c115284f5e623283f1
Ferid Abusov <122831032+Theferid@users.noreply.github.com>
2024-08-29 11:14:46 +0300

    Create send_email.php

---
+++ send_email.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "fridabisov26@gmail.com";
+    $subject = "Yeni İletişim Formu Mesajı";
+    $body = "Ad: $name\nE-posta: $email\nMesaj:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Mesajınız başarıyla gönderildi!";
+    } else {
+        echo "Mesaj gönderilirken bir hata oluştu.";
+    }
+}
+?>
